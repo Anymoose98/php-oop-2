44446c2447f794f1d8fc7fe036c9163aa6529e82
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-31 16:47:45 +0100

    classe base e estensioni fatte

--- index.php
+++ index.php
@@ -1,31 +1,42 @@
 <?php 
     // Definisco la classe prodotti
-    class prodotto{
+    class Prodotto{
         public $titolo;
         public $costo;
         public $foto;
+        public $tipo;
+        public $animale;
 
         function __construct($titolo, $costo , $foto){
             $this->titolo = $titolo;
             $this->costo = $costo;
             $this->foto = $foto;
-        }
-    }
-
-    class dettagliProdotto extends prodotto{
-        public $tipo;
-        public $animale;
-
-        function __construct($titolo, $costo , $foto, $tipo, $animale ){
-            $this->titolo = $titolo;
-            $this->costo = $costo;
-            $this->foto = $foto;
             $this->tipo = $tipo;
             $this->animale = $animale;
-
-
         }
     }
+
+    // Consistenza del mangiare
+    class Consistenza extends Prodotto{
+        public $consistenza;
+
+        function __construct($titolo, $costo , $foto, $tipo, $animale, $consistenza){
+            $this->consistenza = $consistenza;
+        }
+    }
+
+    // Materiale e colore
+    class MaterialeColore extends Prodotto{
+        public $materiale;
+        public $colore;
+
+        function __construct($titolo, $costo , $foto, $tipo, $animale, $materiale, $colore){
+            $this->materiale = $materiale;
+            $this->colore = $colore;
+        }
+    }
+
+
 ?>
 
 <!DOCTYPE html>
